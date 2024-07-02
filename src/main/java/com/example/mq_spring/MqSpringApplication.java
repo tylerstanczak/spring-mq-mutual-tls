4e76fa91ddfad285c338777b3bbe53a15ab924e5
Tyler-Stanczak <tyler.stanczak@ibm.com>
2024-07-02 15:28:54 -0500

    get msgs with jmslistener, use spring jms 6.1.6

--- src/main/java/com/example/mq_spring/MqSpringApplication.java
+++ src/main/java/com/example/mq_spring/MqSpringApplication.java
@@ -9,6 +9,8 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.jms.annotation.JmsListener;
+
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
@@ -19,11 +21,16 @@ public class MqSpringApplication {
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
+	@JmsListener(destination = "DEV.QUEUE.1", containerFactory = "factory2")
+	public void receiveViaJmsListener(String message) {
+		System.out.println("JMS Listener received a message:\n" + message);
+	}
+
 	@GetMapping("/send")
 	String send() {
 		try{
 			jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
-			return "OK";
+			return "SEND - OK";
 		}catch(JmsException ex){
 			ex.printStackTrace();
 			return "FAIL";
@@ -42,20 +49,22 @@ public class MqSpringApplication {
 
 	@PostConstruct
 	public String init() {
-		try{
+		try {
 			jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
-		}catch(JmsException ex){
+			System.out.println("SEND - OK");
+			return "SEND - OK";
+		} catch(JmsException ex){
 			ex.printStackTrace();
 			return "FAIL";
 		}
-		try{
-			String recvMsg = jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
-			System.out.println("ALL OK: " + recvMsg);
-			return "ALL OK";
-		} catch(JmsException ex) {
-			ex.printStackTrace();
-			return "FAIL";
-		}
+		// try{
+		// 	String recvMsg = jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+		// 	System.out.println("MQConnectionFactory RECEIVED MESSAGE:\n" + recvMsg);
+		// 	return "ALL OK";
+		// } catch(JmsException ex) {
+		// 	ex.printStackTrace();
+		// 	return "FAIL";
+		// }
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MqSpringApplication.class, args);
--- src/main/java/com/example/mq_spring/config/JmsConfig.java
+++ src/main/java/com/example/mq_spring/config/JmsConfig.java
@@ -1,20 +1,22 @@
 package com.example.mq_spring.config;
 
-import com.ibm.mq.jms.MQConnectionFactory;
-import com.ibm.msg.client.wmq.WMQConstants;
-
+import com.ibm.mq.jakarta.jms.MQConnectionFactory;
+import com.ibm.msg.client.jakarta.wmq.WMQConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 
-import javax.jms.JMSException;
+import jakarta.jms.JMSException;
+
 @Configuration
 @EnableJms
 public class JmsConfig {
 
-    @Bean
+    @Bean(name="factory1")
     public MQConnectionFactory mqConnectionFactory() throws JMSException {
         MQConnectionFactory mqConnectionFactory = new MQConnectionFactory();
         mqConnectionFactory.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
@@ -23,8 +25,7 @@ public class JmsConfig {
         mqConnectionFactory.setPort(1414);
         mqConnectionFactory.setQueueManager("QM1");
         mqConnectionFactory.setChannel("DEV.APP.SVRCONN");
-        mqConnectionFactory.setStringProperty(WMQConstants.WMQ_SSL_CIPHER_SUITE, "*TLS12");
-        mqConnectionFactory.setBooleanProperty(WMQConstants.USER_AUTHENTICATION_MQCSP, true);
+        mqConnectionFactory.setSSLCipherSuite("*TLS12");
         mqConnectionFactory.setStringProperty(WMQConstants.USERID, "app");
         mqConnectionFactory.setStringProperty(WMQConstants.PASSWORD, "passw0rd");
 
@@ -47,5 +48,12 @@ public class JmsConfig {
     @Bean
     public JmsTemplate jmsTemplate(CachingConnectionFactory cachingConnectionFactory) {
         return new JmsTemplate(cachingConnectionFactory);
+    }
+
+    @Bean(name="factory2")
+    public JmsListenerContainerFactory<?> JmsListenerContainerFactory(MQConnectionFactory mqConnectionFactory) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(mqConnectionFactory);
+        return factory;
     }
 }   
